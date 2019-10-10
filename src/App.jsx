2284244260ccf2095aad1060af8e68e5b44d855f
Khalem <33363455+Khalem@users.noreply.github.com>
2019-10-10 18:48:36 +0100

    Created foundation for music app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App-title">Music from App</div>
+        <div>
+          <input placeholder="Search Artist"/>
+          <button>Submit</button>
+        </div>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
