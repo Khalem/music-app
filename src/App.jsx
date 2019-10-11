21a47cc48737d20dea430d96fdfb87396d099eba
Khalem <33363455+Khalem@users.noreply.github.com>
2019-10-11 18:19:12 +0100

    Connected app to spotify api

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,45 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    const BASE_URL = "https://api.spotify.com/v1/search?";
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: {
+        "Authorization": process.env.API_TOKEN
+      }
+    }).then(response => response.json())
+      .then(json => console.log('json', json));
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Music App</h1>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="Search for an artist"/>
-            <InputGroup.Append>
-              <Button><i class="fas fa-search"></i></Button>
+            <FormControl
+              type="text"
+              placeholder="Search for an artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                }
+              }}/>
+            <InputGroup.Append onClick={() => this.search()}>
+              <Button><i className="fas fa-search"></i></Button>
             </InputGroup.Append>
           </InputGroup>
         </FormGroup>
