fd50a0db514a31a98c557e75ec2472b3cdc90d72
Khalem <33363455+Khalem@users.noreply.github.com>
2019-10-12 19:51:42 +0100

    Created Profile component - search results now displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,62 @@
 import React, {Component} from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
+import Profile from './Profile';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      artist: null,
+      API_TOKEN: ''
     }
   }
 
+  componentDidMount() {
+    /*
+    Get API token from Spotify - it expires so create a new one each time component is mounted to avoid errors.
+    */
+    const FETCH_AUTH = "https://accounts.spotify.com/api/token";
+
+    fetch(FETCH_AUTH, {
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: "Basic Y2I0ZjFkYWRhMDYxNDdhNjlhZGE4Njc2MjY0M2EwZjA6OWNhZTI4NmUxZDg3NGM4NGFhNTA0ZmYwNTJjNDZjNTk=",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    })
+    .then(response => response.json())
+    .then(json => {
+      this.setState({ API_TOKEN: json.access_token });
+      console.log(json);
+    })
+    .catch(error => console.log(error))
+  }
+
   search() {
+    /*
+    Search Spotifys API to fetch result and update state.
+    */
+    console.log(process.env.API_TOKEN);
     const BASE_URL = "https://api.spotify.com/v1/search?";
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     fetch(FETCH_URL, {
       method: 'GET',
       headers: {
-        "Authorization": process.env.API_TOKEN
+        Authorization: `Bearer ${this.state.API_TOKEN}`
       }
     }).then(response => response.json())
-      .then(json => console.log('json', json));
+      .then(json => {
+        const artist = json.artists.items[0];
+        this.setState({
+          artist
+        });
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
 
 
   }
@@ -45,10 +82,9 @@ class App extends Component {
             </InputGroup.Append>
           </InputGroup>
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="Gallery">
           Gallery
         </div>
